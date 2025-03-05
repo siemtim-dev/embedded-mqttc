7fa73efd7ca1cb421a37143d6ace98a63eb34795
Tim Siemers <tim.siemers@outlook.com>
2025-03-05 16:06:46 +0100

    fixed unclear unfase funktion in embassy network impl

--- network/src/embassy.rs
+++ network/src/embassy.rs
@@ -8,6 +8,9 @@ use embedded_io_async::{ErrorKind, ErrorType, Read, ReadReady, Write, WriteReady
 
 use crate::{NetworkConnection, NetworkError, TryRead, TryWrite};
 
+/// struct that contains the rx buffer and tx buffer for the tcp connection
+/// 
+/// the buffers are of size N
 pub struct EmbassyConnectionStackResources<const N: usize> {
     rx_buffer: [u8; N],
     tx_buffer: [u8; N]
@@ -21,14 +24,54 @@ impl <const N: usize> EmbassyConnectionStackResources<N> {
         }
     }
 
+    /// borrow the rx buffer and tx buffer
     pub fn borrow<'a>(&'a mut self) -> EmbassyConnectionResources<'a> {
         EmbassyConnectionResources{
             rx_buffer: &mut self.rx_buffer[..],
             tx_buffer: &mut self.tx_buffer[..]
         }
     }
+
+    /*
+    pub fn borrow2<'a>(&'a mut self) -> EmbassyConnectionResources2<'a, N> {
+        EmbassyConnectionResources2{
+            rx_buffer: &mut self.rx_buffer as *mut u8,
+            tx_buffer: &mut self.tx_buffer as *mut u8,
+            _phantom_data: PhantomData
+        }
+    }
+    */
 }
 
+/*
+pub struct EmbassyConnectionResources2<'a, const N: usize> {
+    rx_buffer: *mut u8,
+    tx_buffer: *mut u8,
+    _phantom_data: PhantomData<&'a mut u8>
+}
+
+impl <'a, const N: usize> EmbassyConnectionResources2<'a, N> {
+
+    pub fn unwrap(&'a mut self) -> (&'a mut [u8], &'a mut [u8]) {
+        unsafe {
+            (
+                slice::from_raw_parts_mut(self.rx_buffer, N),
+                slice::from_raw_parts_mut(self.tx_buffer, N)
+            )
+        }
+    }
+
+    unsafe fn unwrap_static(&self) -> (&'static mut [u8], &'static mut [u8]) {
+        (
+            slice::from_raw_parts_mut(self.rx_buffer, N),
+            slice::from_raw_parts_mut(self.tx_buffer, N)
+        )
+
+    }
+}
+*/
+
+/// struct that contains the pointers to the rx buffer and tx buffer
 pub struct EmbassyConnectionResources<'a> {
     rx_buffer: &'a mut [u8],
     tx_buffer: &'a mut [u8]
@@ -36,19 +79,22 @@ pub struct EmbassyConnectionResources<'a> {
 
 impl <'a> EmbassyConnectionResources<'a> {
 
-    fn borrow<'b>(&'b mut self) -> (&'b mut [u8], &'b mut [u8]) {
+    fn unwrap<'b>(&'b mut self) -> (&'b mut [u8], &'b mut [u8]) {
         (self.rx_buffer, self.tx_buffer)
     }
 
-    fn borrowx(&mut self) -> (&'static mut [u8], &'static mut [u8]) {
-        let ( rx_buffer, tx_buffer ) = self.borrow();
-        unsafe {
+    /// unwrap the rx buffer and tx buffer as static references
+    /// 
+    /// # Safety
+    /// 
+    /// Concurrent write operations are possible. The user of this function must avoid them.
+    unsafe fn unwrap_unsafe(&mut self) -> (&'a mut [u8], &'a mut [u8]) {
+        let ( rx_buffer, tx_buffer ) = self.unwrap();
             
-            (
-                mem::transmute(rx_buffer),
-                mem::transmute(tx_buffer)
-            )
-        }
+        (
+            mem::transmute(rx_buffer),
+            mem::transmute(tx_buffer)
+        )
     }
 }
 
@@ -77,8 +123,19 @@ impl <'a> EmbassyNetworkConnection<'a> {
             let result = dns_client.query(hostname, DnsQueryType::Aaaa).await;
 
             match result {
-                Ok(addrs) => Ok(addrs.into_iter().next()),
-                Err(embassy_net::dns::Error::InvalidName) => Ok(None),
+                Ok(addrs) => {
+                    if let Some(addr) = addrs.into_iter().next() {
+                        info!("dns aaaa: {} -> {}", hostname, addr);
+                        Ok(Some(addr))
+                    } else {
+                        info!("dns aaaa: {} -> nothing", hostname);
+                        Ok(None)
+                    }
+                },
+                Err(embassy_net::dns::Error::InvalidName) => {
+                    info!("dns aaaa: {} -> invalid name", hostname);
+                    Ok(None)
+                },
                 Err(e) => Err(e)
             }
         };
@@ -87,8 +144,19 @@ impl <'a> EmbassyNetworkConnection<'a> {
             let result = dns_client.query(hostname, DnsQueryType::A).await;
 
             match result {
-                Ok(addrs) => Ok(addrs.into_iter().next()),
-                Err(embassy_net::dns::Error::InvalidName) => Ok(None),
+                Ok(addrs) => {
+                    if let Some(addr) = addrs.into_iter().next() {
+                        info!("dns a: {} -> {}", hostname, addr);
+                        Ok(Some(addr))
+                    } else {
+                        info!("dns a: {} -> nothing", hostname);
+                        Ok(None)
+                    }
+                }
+                Err(embassy_net::dns::Error::InvalidName) => {
+                    info!("dns a: {} -> invalid name", hostname);
+                    Ok(None)
+                },
                 Err(e) => Err(e)
             }
         };
@@ -124,7 +192,7 @@ impl <'a> EmbassyNetworkConnection<'a> {
         };
         
         info!("connecting...");
-        let _connection = socket.connect(endpoint).await.map_err(|e| {
+        socket.connect(endpoint).await.map_err(|e| {
             error!("tcp connection failed: {}", e);
             NetworkError::from(e)
         })?;
@@ -182,7 +250,9 @@ impl <'a> TryWrite for EmbassyNetworkConnection<'a> {
 
 impl <'a> NetworkConnection for EmbassyNetworkConnection<'a> {
     async fn connect(&mut self) -> Result<(), crate::NetworkError> {
-        let ( rx_buffer, tx_buffer ) = self.resources.borrowx();
+        let ( rx_buffer, tx_buffer ) = unsafe {
+            self.resources.unwrap_unsafe()
+        };
         self.connect_intern(rx_buffer, tx_buffer).await
     }
 }
